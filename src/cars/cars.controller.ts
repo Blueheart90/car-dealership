ed212b36d79dd1a61a1f64ca18c0495b2d67b309
Jesus david <jbernalme@gmail.com>
2024-07-23 15:11:35 -0500

    add controller

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
+
+@Controller('cars')
+export class CarsController {
+  constructor(private readonly carsService: CarsService) {}
+
+  @Get(':id')
+  getCarById(@Param('id', ParseUUIDPipe) id: string) {
+    console.log({ id });
+
+    return this.carsService.findOneById(id);
+  }
+
+  @Get()
+  getAllCars() {
+    return this.carsService.findAll();
+  }
+
+  @Post()
+  createCar(@Body() createCarDto: CreateCarDto) {
+    return createCarDto;
+  }
+  @Patch(':id')
+  updateCar(@Param('id', ParseIntPipe) id: number, @Body() body: any) {
+    return body;
+  }
+  @Delete(':id')
+  deleteCar(@Param('id', ParseIntPipe) id: number) {
+    return { msg: `Car ${id} was deleted` };
+  }
+}
